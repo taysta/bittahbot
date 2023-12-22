560d0a2a5c4ed47e37c3d7121d0800a15927e9a9
taysta <taylor.brough13@gmail.com>
2023-12-22 18:36:58 +0800

    Formatting, warnings

--- schemas/general_schema.py
+++ schemas/general_schema.py
@@ -1,40 +1,47 @@
 from includes import mongo
 import datetime
 
+
 def check_setup_exists(user):
-    if mongo.db['Messages'].count_documents({"userId":user.id}) < 1:
+    if mongo.db['Messages'].count_documents({"userId": user.id}) < 1:
         return False
     return True
 
+
 def add_profile_setup(user, uniqueue_id, stage):
     data = {
-        "userId":user.id,
-        "uniqueueId":uniqueue_id,
-        "created":datetime.datetime.now(),
-        "stage":stage
+        "userId": user.id,
+        "uniqueueId": uniqueue_id,
+        "created": datetime.datetime.now(),
+        "stage": stage
     }
 
     mongo.db['Messages'].insert_one(data)
 
-def check_custom_id_exists(id):
-    if mongo.db['Messages'].find_one({"uniqueueId":id}):
+
+def check_custom_id_exists(check_id):
+    if mongo.db['Messages'].find_one({"uniqueueId": check_id}):
         return True
     return False
 
-def get_message_stage(id):
-    return mongo.db['Messages'].find_one({"uniqueueId":id})['stage']
 
-def update_stage(id, user, stage):
+def get_message_stage(stage_id):
+    return mongo.db['Messages'].find_one({"uniqueueId": stage_id})['stage']
+
+
+def update_stage(stage_id, user, stage):
     data = {
         "$set": {
-            "stage":stage
+            "stage": stage
         }
     }
 
-    mongo.db['Messages'].update_one({"uniqueueId":id, "userId":user.id}, data)
+    mongo.db['Messages'].update_one({"uniqueueId": stage_id, "userId": user.id}, data)
 
-def remove_setup(id):
-    mongo.db['Messages'].delete_one({"uniqueueId":id})
+
+def remove_setup(remove_id):
+    mongo.db['Messages'].delete_one({"uniqueueId": remove_id})
+
 
 def get_all_setup_messages():
     return mongo.db['Messages'].find({})
